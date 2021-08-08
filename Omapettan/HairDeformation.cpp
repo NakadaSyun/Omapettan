ef50d60137c65225479a2045e05326922d65a3c6
DRQE <https://github.com/DRQE/mamimumema>
2021-08-09 04:42:28 +0900

    HairDeformation（毛の変形）追加

---
+++ Omapettan/HairDeformation.cpp
@@ -0,0 +1,10 @@
+#include "HairDeformation.h"
+#include "HairVertexBuffer.h"
+
+c_HairD::c_HairD() {
+
+}
+
+void c_HairD::f_Change() {
+	//MV1SetScale(modelHandle, VGet(1.0f, 2.0f, 1.0f)); //モデルの形を変える（今はY座標にスケールを２倍に）
+}
---
+++ Omapettan/HairDeformation.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "DxLib.h"
+
+class c_HairD {
+public:
+	c_HairD();
+	void f_Change();
+private:
+};
