b56df5a599e13ad4fdba4198ae239d8423525a44
DRQE <https://github.com/DRQE/mamimumema>
2021-08-19 14:56:04 +0900

    ランダム配置＆複数個配置できるように

--- Omapettan/DrawAcne.h
+++ Omapettan/DrawAcne.h
@@ -4,6 +4,19 @@
 #include "Collision.h"
 #include <math.h>
 
+namespace {
+	const int ACNE_NUM = 4;
+
+	// ステージ(腕)の太さの半径
+	const float ARM_RADIUS_ACNE = 760.0f - 10.0f;
+
+	// ステージ(腕)の長さ
+	const float ARM_LENGTH_ACNE = 7000.0f;
+
+	const float ARM_ADJUST_POS_ACNE = 1000.0f;
+
+}
+
 class c_Acne{
 public:
 	c_Acne(const int Acne_Model,const int Acne_Graph);
@@ -17,4 +30,7 @@ public:
 private:
 	int modelacne;
 	int graphacne;
+	int acneStatus[ACNE_NUM];           // 各できものの状態
+	float acnepersonalRotation[ACNE_NUM];   // 各できものの角度
+	float acnepersonalPosZ[ACNE_NUM];
 };
